44c1c30689018fadc7cfba1d1f525469254ec366
omardun <omar.rosquero10@gmail.com>
2022-08-10 10:45:16 -0400

    update

--- seeders/20220810134550-StickerDB.js
+++ seeders/20220810134550-StickerDB.js
@@ -3,78 +3,90 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(
-      'stickers',
+      'StickerDBs',
       [
         {
           name: 'Bucs Sticker',
           price: 2.00,
           shape: 'square',
+          image: 'cookie.jpeg',
           createdAt: new Date(),
           updatedAt: new Date()
         }, {
           name: 'Cubs Sticker',
           price: 2.50,
           shape: 'round',
+          image: 'cookie.jpeg',
           createdAt: new Date(),
           updatedAt: new Date()
         }, {
           name: 'Tupac Sticker',
           price: 2.00,
           shape: 'square',
+          image: 'cookie.jpeg',
           createdAt: new Date(),
           updatedAt: new Date()
         }, {
           name: 'Bad Bunny Sticker',
           price: 3.00,
           shape: 'rectangle',
+          image: 'cookie.jpeg',
           createdAt: new Date(),
           updatedAt: new Date()
         }, {
           name: 'Kobe Sticker',
           price: 2.50,
           shape: 'round',
+          image: 'cookie.jpeg',
           createdAt: new Date(),
           updatedAt: new Date()
         }, {
           name: 'Jordan Sticker',
           price: 3.00,
           shape: 'rectangle',
+          image: 'cookie.jpeg',
           createdAt: new Date(),
           updatedAt: new Date()
         }, {
           name: '?(._.)? Sticker',
           price: 2.00,
           shape: 'square',
+          image: 'cookie.jpeg',
           createdAt: new Date(),
           updatedAt: new Date()
         }, {
           name: 'Mike Wazoski Sticker',
           price: 2.00,
           shape: 'square',
+          image: 'cookie.jpeg',
           createdAt: new Date(),
           updatedAt: new Date()
         }, {
           name: 'Ben Tennyson Sticker',
           price: 2.50,
           shape: 'round',
+          image: 'cookie.jpeg',
           createdAt: new Date(),
           updatedAt: new Date()
         }, {
           name: 'Ed, Edd n Eddy Sticker',
           price: 3.00,
           shape: 'rectangle',
+          image: 'cookie.jpeg',
           createdAt: new Date(),
           updatedAt: new Date()
         }, {
           name: 'Cat Sticker',
           price: 2.00,
           shape: 'square',
+          image: 'cookie.jpeg',
           createdAt: new Date(),
           updatedAt: new Date()
         }, {
           name: 'Rocket League car Sticker',
           price: 2.50,
           shape: 'round',
+          image: 'cookie.jpeg',
           createdAt: new Date(),
           updatedAt: new Date()
         }
@@ -83,4 +95,4 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('stickers', null, {});
   }
-}; 
+};
